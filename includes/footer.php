6e701239a056914824a984512823d841cad72c61
GonApXc <gomez.gonzalo73823@esetp760.edu.ar>
2025-10-24 13:54:50 -0300

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+<footer>
+	<div class="container">
+		<span class="text-muted">&copy; <?php echo date('Y'); ?></span>
+	</div>
+</footer>
+
+<script type="text/javascript" src=""></script>    <!------link boostrap----->
+
+</body>
+</html>
---
+++ includes/nav.php
@@ -0,0 +1,14 @@
+<nav class="site-header sticky-top py-2">
+	<div class="container d-flex flex-column">
+		<a href="admin.php">Volver A Principal</a>
+
+		<a class="py-2" href="adminMarcas.php">marcas</a>
+		<a class="py-2" href="adminCategorias.php">Categorias</a>
+		<a class="py-2" href="adminProductos.php">Productos</a>
+		<a class="py-2" href="adminUsuarios.php">Usuarios</a>
+
+		<button class="btn btn-dark">
+			<a href="formLogin.php"><li class="fas fa-sign-in">Ingresar</li></a>
+		</button>
+	</div>
+</nav>
